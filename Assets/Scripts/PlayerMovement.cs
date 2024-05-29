20502f905ce2ebf409bc575ad448fcab4d263c4e
JustinBale55 <justinbale55@hotmail.com>
2024-05-29 17:49:34 -0600

    Playing with movement and sprites
    Everything here is just experimental

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,28 +7,57 @@ public class PlayerMovement : MonoBehaviour
 {
 	[Header("References")]
 	[SerializeField]
-	InputActionAsset inputActions;
+	private InputActionAsset inputActions;
 	[SerializeField]
-	Rigidbody rb;
+	private Rigidbody2D rb;
+	[SerializeField]
+	private Animator anim;
 
 	private InputAction moveAction;
 	private Vector2 moveValue;
+	public int facingDirection = 1;
+
+	[Header("Changable Values")]
+	public float speed = 5;
+
 
 	private void Awake()
 	{
 		moveAction = inputActions.FindActionMap("Gameplay").FindAction("Movement");
 	}
 
-	// Start is called before the first frame update
-	void Start()
+	// FixedUpdate is called 50x frame
+    void FixedUpdate()
     {
-        
+		// Capture the input value
+        moveValue = moveAction.ReadValue<Vector2>();
+
+		if (moveValue.x > 0 && transform.localScale.x < 0 ||
+			moveValue.x < 0 && transform.localScale.x > 0)
+		{
+			Flip();
+		}
+
+		anim.SetFloat("Horizontal", Mathf.Abs(moveValue.x)); // Convert x value into postive
+		anim.SetFloat("Vertical", Mathf.Abs(moveValue.y));// Convert y value into postive
+
+		// Move the rigidbody using the moveValue
+		//rb.velocity = new Vector2(moveValue.x, moveValue.y) * speed;
+		rb.MovePosition(rb.position +  moveValue * speed * Time.fixedDeltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveValue = moveAction.ReadValue<Vector2>();
-		Debug.Log(moveValue);
-    }
+	void Flip()
+	{
+		facingDirection *= -1;
+		transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+	}
+
+	private void OnEnable()
+	{
+		moveAction.Enable();
+	}
+	private void OnDisable()
+	{
+		moveAction.Disable();
+	}
 }
